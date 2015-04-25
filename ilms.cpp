57d54516b88a9dce84d91c6e76ff4396a859f027
Yukariko <rkdals7979@naver.com>
2015-04-26 05:55:43 +0900

    v 0.45
    bug fix

--- ilms.cpp
+++ ilms.cpp
@@ -1,6 +1,23 @@
 #include "ilms.h"
 
 /*
+ * 테스트 해시 함수
+ */
+
+long long test(long long data){return data*997*1499;}
+long long test2(long long data){return data*1009*1361;}
+long long test3(long long data){return data*1013*1327;}
+long long test4(long long data){return data*1013*1327;}
+long long test5(long long data){return data*1013*1327;}
+long long test6(long long data){return data*1013*1327;}
+long long test7(long long data){return data*1013*1327;}
+long long test8(long long data){return data*1013*1327;}
+long long test9(long long data){return data*1013*1327;}
+long long test10(long long data){return data*1013*1327;}
+long long test11(long long data){return data*1013*1327;}
+
+
+/*
  * Ilms 생성자
  * 설정들을 불러오고
  * 블룸필터를 초기화 해줌
--- ilms.h
+++ ilms.h
@@ -16,20 +16,4 @@ private:
 	Bloomfilter *childFilter;
 };
 
-/*
- * 테스트 해시 함수
- */
-
-long long test(long long data){return data*997*1499;}
-long long test2(long long data){return data*1009*1361;}
-long long test3(long long data){return data*1013*1327;}
-long long test4(long long data){return data*1013*1327;}
-long long test5(long long data){return data*1013*1327;}
-long long test6(long long data){return data*1013*1327;}
-long long test7(long long data){return data*1013*1327;}
-long long test8(long long data){return data*1013*1327;}
-long long test9(long long data){return data*1013*1327;}
-long long test10(long long data){return data*1013*1327;}
-long long test11(long long data){return data*1013*1327;}
-
 #endif
