967abcd7f5532eafe418a3c5782ef9084393eead
Yukariko <rkdals7979@naver.com>
2015-05-04 19:58:44 +0900

    v 1.31
    add client input

--- client/main.cpp
+++ client/main.cpp
@@ -1,21 +1,55 @@
 #include <iostream>
+#include <cstdio>
 
 #include "ilmscli.h"
 
 int main()
 {
-	IlmsCli ilms("210.117.184.166");
+	char cmd[BUF_SIZE];
+	IlmsCli ilms("0");
+	while(scanf("%s",cmd) == 1)
+	{
+		char value[BUF_SIZE];
 
-	char buf[BUF_SIZE];
+		scanf("%s",value);
 
-	int len;
+		if(strcmp(cmd, "IP") == 0)
+		{
+			ilms.setIp(value);
+		}
 
-	ilms.req_data_add(1234LL,"210.117.184.166");
+		else if(strcmp(cmd, "SET") == 0)
+		{
+			long long data = *(long long *)value;
+			scanf("%s",value);
+			ilms.req_data_add(data, value);
+		}
 
-	len = ilms.req_data_search(1234LL, buf);
+		else if(strcmp(cmd, "GET") == 0)
+		{
+			char buf[BUF_SIZE];
+			long long data = *(long long *)value;
+			int len = ilms.req_data_search(data, buf);
 
-	if(len > 0)
-		std::cout << buf << std::endl;
+			if(len > 0)
+				std::cout << buf << std::endl;
+		}
 
+		else if(strcmp(cmd, "DELETE") == 0)
+		{
+			long long data = *(long long *)value;
+			ilms.req_data_delete;
+		}
+
+		else if(strcmp(cmd, "EXIT") == 0)
+		{
+			break;
+		}
+
+		else
+		{
+			std::cout << "ERROR" << std::endl;
+		}
+	}
 	return 0;
 }
