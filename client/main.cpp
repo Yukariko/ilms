3d05fee63ca6f4da540f16bdda5a88b27ccec66c
Yukariko <rkdals7979@naver.com>
2015-05-04 20:09:54 +0900

    v 1.33
    bug fix

--- client/main.cpp
+++ client/main.cpp
@@ -11,33 +11,36 @@ int main()
 	while(scanf("%s",cmd) == 1)
 	{
 		char value[BUF_SIZE];
-
-		if(scanf("%s",value) != 1)
-		{
-			std::cout << "ERROR" << std::endl;
-			exit(1);
-		}
-
+		long long data;
 		if(strcmp(cmd, "IP") == 0)
-		{
-			ilms.setIp(value);
-		}
-
-		else if(strcmp(cmd, "SET") == 0)
-		{
-			long long data = *(long long *)value;
+		{	
 			if(scanf("%s",value) != 1)
 			{
 				std::cout << "ERROR" << std::endl;
 				exit(1);
 			}
+			ilms.setIp(value);
+		}
+
+		else if(strcmp(cmd, "SET") == 0)
+		{
+			if(scanf("%lld %s",&data,value) != 2)
+			{
+				std::cout << "ERROR" << std::endl;
+				exit(1);
+			}
+
 			ilms.req_data_add(data, value);
 		}
 
 		else if(strcmp(cmd, "GET") == 0)
 		{
 			char buf[BUF_SIZE];
-			long long data = *(long long *)value;
+			if(scanf("%lld",&data) != 1)
+			{
+				std::cout << "ERROR" << std::endl;
+				exit(1);
+			}
 			int len = ilms.req_data_search(data, buf);
 
 			if(len > 0)
@@ -46,7 +49,11 @@ int main()
 
 		else if(strcmp(cmd, "DELETE") == 0)
 		{
-			long long data = *(long long *)value;
+			if(scanf("%lld",&data) != 1)
+			{
+				std::cout << "ERROR" << std::endl;
+				exit(1);
+			}
 			ilms.req_data_delete(data);
 		}
 
