a072e732c7a88e915b152fcab0d6d3a737fe9888
Yukariko <rkdals7979@naver.com>
2015-07-15 22:18:31 +0900

    v 7.52

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "Usage: ./build.sh (CPU|GPU)"
+    exit
+fi
+
 if [ $1 == "CPU" ]; then
-	make -C ./cpu clean
-	make -C ./cpu
-	mv ./cpu/ilms ./
+    make -C ./cpu clean
+    make -C ./cpu
+    mv ./cpu/ilms ./
 elif [ $1 == "GPU" ]; then
-	make -C ./gpu clean
-	make -C ./gpu
-	mv ./gpu/ilms ./
+    make -C ./gpu clean
+    make -C ./gpu
+    mv ./gpu/ilms ./
 else
-	echo "Usage: ./build.sh (CPU|GPU)"
+    echo "Usage: ./build.sh (CPU|GPU)"
 fi
