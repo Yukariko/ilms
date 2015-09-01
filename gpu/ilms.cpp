89174648df5c00d6b183a73199d06622c5495089
Yukariko <rkdals7979@naver.com>
2015-09-01 22:15:27 +0900

    fix

--- gpu/ilms.cpp
+++ gpu/ilms.cpp
@@ -699,7 +699,11 @@ void Ilms::req_id_register(unsigned long ip_num)
 		return;
 
 	my_filter->insert(data);
-	insert(data,DATA_SIZE,value,*(unsigned char *)(value-1));
+
+	string loc = ":";
+	loc += value;
+
+	insert(data,DATA_SIZE, loc.c_str(), loc.size());
 
 	if(global_switch == MYREFRESH)
 		shadow_filter->insert(data);
