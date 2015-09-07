d4ae17cc82fe73cbb5bf203db2fe5b1c94a130c6
Yukariko <rkdals7979@naver.com>
2015-09-07 19:09:40 +0900

    fix

--- client/main.cpp
+++ client/main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 const char *oper[] = {"IP","REG","SET","GET","REP","SUB","DELETE","EXIT",NULL};
 enum {IP,REG,SET,GET,REP,SUB,DELETE,EXIT,ERROR};
 
-
 int main()
 {
 	string cmd;
@@ -37,19 +36,19 @@ int main()
 			{
 				getline(cin, value);
 				if(value.size())
-					ret = ilms.req_id_register(EID(data), value.substr(1));
+					ret = ilms.req_id_register(data, value.substr(1));
 				else
-					ret = ilms.req_id_register(EID(data), value);
+					ret = ilms.req_id_register(data, value);
 			}
 			break;
 		case SET:
 			if(cin >> data >> value)
-				ret = ilms.req_loc_update(0, EID(data), value);
+				ret = ilms.req_loc_update(0, data, value);
 			break;
 		case GET:
 			if(cin >> data)
 			{
-				len = ilms.req_lookup(EID(data), value);
+				len = ilms.req_lookup(data, value);
 				if(len == -1)
 					cout << "No ID" << endl;
 				else if(len < 2)
@@ -60,15 +59,15 @@ int main()
 			break;
 		case REP:
 			if(cin >> data >> value)
-				ret = ilms.req_loc_update(2, EID(data), value);
+				ret = ilms.req_loc_update(2, data, value);
 			break;
 		case SUB:
 			if(cin >> data >> value)
-				ret = ilms.req_loc_update(1, EID(data), value);
+				ret = ilms.req_loc_update(1, data, value);
 			break;
 		case DELETE:
 			if(cin >> data)
-				ret = ilms.req_id_deregister(EID(data));
+				ret = ilms.req_id_deregister(data);
 			break;
 		case EXIT:
 			return 0;
