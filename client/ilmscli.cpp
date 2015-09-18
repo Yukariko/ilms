18a70b8f72bc3c82a38dcb050098e01e184bf12c
Yukariko <rkdals7979@naver.com>
2015-09-18 21:39:51 +0900

    fix

--- client/ilmscli.cpp
+++ client/ilmscli.cpp
@@ -12,6 +12,9 @@
  * Ilms 클라이언트 생성자
  * 요청을 전달할 노드의 ip를 설정하고 자신의 소켓 초기화
  */
+const char* modes[] = {
+	"GET", "SET", "SUB", "REP"
+};
 
 IlmsCli::IlmsCli(string ip)
 {
@@ -46,9 +49,40 @@ void IlmsCli::set_ip(const string& ip)
 	this->ip = ip;
 }
 
+int IlmsCli::print_response(char *buf, int len)
+{
+	if(len == 1 + ID_SIZE + 1)
+	{
+		std::cout << "ID : " << buf+1 << ", ";
+		std::cout << (buf[1+ID_SIZE] == 0? "REG " : "DEL ");
+		std::cout << (buf[0] == REQ_SUCCESS? "Success" : "Fail") << std::endl;
+		return buf[1+ID_SIZE] == 0? REQ_ID_REGISTER: REQ_ID_DEREGISTER;
+	}
+	else
+	{
+		std::cout << "ID : " << buf+1 << ", ";
+		int mode = buf[1+ID_SIZE];
+		if(mode)
+		{
+			if(buf[1+ID_SIZE+1])
+				std::cout << "LOC : " << buf+1+ID_SIZE+1+1 << ", ";
+			std::cout << modes[mode] << " " << (header[0] == REQ_SUCCESS? "Success" : "Fail") << std::endl;
+		}
+		else
+		{
+			if(header[0] == REQ_FAIL)
+				std::cout << modes[0] << " No ID" << std::endl;
+			else
+				std::cout << "LOC : " << (buf[1+ID_SIZE+1] < 2? "No LOC" : buf+1+ID_SIZE+1+1) << std::endl;
+		}
+		return mode;
+	}
+}
+
 bool IlmsCli::req_id_register(const string& id, const string& loc)
 {
 	char header[BUF_SIZE];
+	char response[BUF_SIZE];
 	int len=0;
 
 	header[len++] = REQ_ID_REGISTER;
@@ -66,13 +100,41 @@ bool IlmsCli::req_id_register(const string& id, const string& loc)
 	len += header[len] + 1;
 
 	this->send(header,len);
-	this->recieve(header);
-
-	std::cout << "ID : " << id << ", ";
-	if(loc.length())
-		std::cout << "LOC : " << loc << ", ";
-	std::cout << "REG " << (header[0] == REQ_SUCCESS? "Success" : "Fail") << std::endl;
-	return header[0] == REQ_SUCCESS;
+	for(int i=1; i < RETRANSMISSION_FREQUENCY; i++)
+	{
+		int rlen = this->recieve(response);
+		if(rlen > 0)
+		{
+			int prot = print_response(response, rlen);
+			if(prot == REQ_ID_REGISTER)
+			{
+				bool find = true;
+				for(size_t i=0; i < ID_SIZE; i++)
+				{
+					if(i < id.size() && id[i] != response[1+i])
+					{
+						find = false;
+						break;
+					}
+					else if(i >= id.size() && response[1+i])
+					{
+						find = false;
+						break;
+					}
+				}
+				if(find)
+					return 1;
+				i--;
+			}
+		}
+		else
+		{
+			std::cout << "ID : " << id << ", REG Retransmission..." << std::endl;
+			this->send(header,len);
+		}
+	}
+	std::cout << "ID : " << id << ", REG Fail" << std::endl;
+	return false;
 }
 
 
@@ -80,13 +142,10 @@ bool IlmsCli::req_id_register(const string& id, const string& loc)
  * 데이터 추가 요청
  */
 
-const char* modes[] = {
-	"GET", "SET", "SUB", "REP"
-};
-
 int IlmsCli::req_loc_update(char mode, const string& id, const string& loc)
 {
 	char header[BUF_SIZE];
+	char response[BUF_SIZE];
 	int len=0;
 
 	header[len++] = REQ_LOOKUP;
@@ -104,17 +163,50 @@ int IlmsCli::req_loc_update(char mode, const string& id, const string& loc)
 	len += header[len] + 1;
 
 	this->send(header,len);
+	for(int i=1; i < RETRANSMISSION_FREQUENCY; i++)
+	{
+		int rlen = this->recieve(response);
+		if(rlen > 0)
+		{
+			int prot = print_response(response, rlen);
+			if(mode == prot)
+			{
+				bool find = true;
+				for(size_t i=0; i < ID_SIZE; i++)
+				{
+					if(i < id.size() && id[i] != response[1+i])
+					{
+						find = false;
+						break;
+					}
+					else if(i >= id.size() && response[1+i])
+					{
+						find = false;
+						break;
+					}
+				}
 
-	int ret = this->recieve(header);
-
-	std::cout << "ID : " << id << ", ";
-	if(loc.length())
-		std::cout << "LOC : " << loc << ", ";
-	std::cout << modes[mode] << " " << (header[0] == REQ_SUCCESS? "Success" : "Fail") << std::endl;
-
-	if(ret < 0)
-		return 0;
-	return header[0] == REQ_SUCCESS? 1: 0;
+				for(size_t i=0; i < loc.size(); i++)
+				{
+					if(response[1+ID_SIZE+2 + i] != loc[i])
+					{
+						find = false;
+						break;
+					}
+				}
+				if(find)
+					return 1;
+				i--;
+			}
+		}
+		else
+		{
+			std::cout << "ID : " << id << ", LOC : " << loc << " " << modes[mode] << " Retransmission..." << std::endl;
+			this->send(header,len);
+		}
+	}
+	std::cout << "ID : " << id << ", LOC : " << loc << " " << modes[mode] << " Fail" << std::endl;
+	return false;
 }
 
 /*
@@ -122,9 +214,10 @@ int IlmsCli::req_loc_update(char mode, const string& id, const string& loc)
  * 검색 결과를 받을때까지 기다림
  */
 
-int IlmsCli::req_lookup(const string& id, string& buf)
+int IlmsCli::req_lookup(const string& id)
 {
 	char header[BUF_SIZE];
+	char response[BUF_SIZE];
 	int len=0;
 
 	header[len++] = REQ_LOOKUP;
@@ -141,25 +234,42 @@ int IlmsCli::req_lookup(const string& id, string& buf)
 
 
 	this->send(header,len);
-	len = this->recieve(header);
-
-	std::cout << "ID : " << id << ", ";
-	if(len < 0)
+	for(int i=1; i < RETRANSMISSION_FREQUENCY; i++)
 	{
-		std::cout << modes[0] << " Fail" << std::endl;
-		return -2;
-	}
+		int rlen = this->recieve(response);
+		if(rlen > 0)
+		{
+			int prot = print_response(response, rlen);
+			if(mode == prot)
+			{
+				bool find = true;
+				for(size_t i=0; i < ID_SIZE; i++)
+				{
+					if(i < id.size() && id[i] != response[1+i])
+					{
+						find = false;
+						break;
+					}
+					else if(i >= id.size() && response[1+i])
+					{
+						find = false;
+						break;
+					}
+				}
 
-	if(header[0] == REQ_FAIL)
-	{
-		std::cout << modes[0] << " No ID" << std::endl;
-		return -1;
+				if(find)
+					return 1;
+				i--;
+			}
+		}
+		else
+		{
+			std::cout << "ID : " << id << modes[0] << " Retransmission..." << std::endl;
+			this->send(header,len);
+		}
 	}
-
-	header[len] = 0;
-	buf = header + ID_SIZE + 3;
-	std::cout << "LOC : " << (buf.length() < 2? "No LOC" : buf) << std::endl;
-	return buf.length();
+	std::cout << "ID : " << id << modes[0] << " Fail" << std::endl;
+	return len;
 }
 
 /*
@@ -169,6 +279,7 @@ int IlmsCli::req_lookup(const string& id, string& buf)
 bool IlmsCli::req_id_deregister(const string& id)
 {
 	char header[BUF_SIZE];
+	char response[BUF_SIZE];
 	int len=0;
 
 	header[len++] = REQ_ID_DEREGISTER;
@@ -181,12 +292,41 @@ bool IlmsCli::req_id_deregister(const string& id)
 	}
 
 	this->send(header,len);
-	this->recieve(header);
-
-	std::cout << "ID : " << id << ", ";
-	std::cout << "DEL " << (header[0] == REQ_SUCCESS? "Success" : "Fail") << std::endl;
-
-	return header[0] == REQ_SUCCESS;
+	for(int i=1; i < RETRANSMISSION_FREQUENCY; i++)
+	{
+		int rlen = this->recieve(response);
+		if(rlen > 0)
+		{
+			int prot = print_response(response, rlen);
+			if(prot == REQ_ID_DEREGISTER)
+			{
+				bool find = true;
+				for(size_t i=0; i < ID_SIZE; i++)
+				{
+					if(i < id.size() && id[i] != response[1+i])
+					{
+						find = false;
+						break;
+					}
+					else if(i >= id.size() && response[1+i])
+					{
+						find = false;
+						break;
+					}
+				}
+				if(find)
+					return 1;
+				i--;
+			}
+		}
+		else
+		{
+			std::cout << "ID : " << id << ", DEL Retransmission..." << std::endl;
+			this->send(header,len);
+		}
+	}
+	std::cout << "ID : " << id << ", DEL Fail" << std::endl;
+	return false;
 }
 
 /*
--- client/ilmscli.h
+++ client/ilmscli.h
@@ -12,6 +12,7 @@
 #define ID_SIZE 24
 
 #define TIME_LIMIT	5
+#define RETRANSMISSION_FREQUENCY 5
 #define LOC_LOOKUP						0x00
 #define LOC_SET								0x01
 #define LOC_SUB								0x02
@@ -29,10 +30,11 @@ public:
 	bool req_id_register(const string& id, const string& loc);
 	int req_loc_update(char mode, const string& id, const string& loc);
 	bool req_id_deregister(const string& id);
-	int req_lookup(const string& id, string& buf);
+	int req_lookup(const string& id);
 
 	void send(const char *buf,int len);
 	int recieve(char *buf);
+	int print_response(char *buf, int len);
 	void error_handling(const char *message);
 
 private:
